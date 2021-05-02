e6ae0aa24299b3e49dd65d2a4a6679584c4d5104
Gurjit Singh <gurjit.singh.engineer@gmail.com>
2021-05-02 16:18:46 +0100

    Add Collection ViewModel

---
+++ ImageGalleryApp/ViewModel/CollectionViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  CollectionViewModel.swift
+//  ImageGalleryApp
+//
+//  Created by Gurjit Singh on 02/05/21.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewModel {
+    let constants = Constants()
+    var pageCount = 0
+    var tags = "kitten"
+    var imageData = [FlickrUrls]()
+    let loadingIndicator = UIActivityIndicatorView(style: .large)
+    let fetchImages = FetchFlickrImages()
+    let imageSource = FlickrImageSource()
+}
+
+extension CollectionViewModel {
+
+    func numberOfRows(frameWidth: CGFloat) -> CGSize {
+        let padding = constants.sectionInsets.left * (constants.itemsPerRow + 1)
+        let width = frameWidth - padding
+        let widthPerImage = width / constants.itemsPerRow
+        return CGSize(width: widthPerImage, height: widthPerImage)
+    }
+
+    var resetImagesData : Void {
+        imageData.removeAll()
+        tags = ""
+        pageCount = 0
+    }
+}
