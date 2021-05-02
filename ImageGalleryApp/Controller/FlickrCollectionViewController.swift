63809227c610cf899f1608fa55ad73784003be47
Gurjit Singh <gurjit.singh.engineer@gmail.com>
2021-05-02 16:20:09 +0100

    Move code to view model

--- ImageGalleryApp/Controller/FlickrCollectionViewController.swift
+++ ImageGalleryApp/Controller/FlickrCollectionViewController.swift
@@ -9,48 +9,19 @@ import UIKit
 
 class FlickrCollectionViewController: UICollectionViewController {
 
-    var imageData = [FlickrUrls]()
-    let constants = Constants()
-    let fetchImages = FetchFlickrImages()
-    var tags = "kitten"
-    var pageCount = 0
-    let imageSource = FlickrImageSource()
-    let loadingIndicator = UIActivityIndicatorView(style: .large)
-    let jsonDecoder = JSONDecoder()
+    private var viewModel = CollectionViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(viewModel.loadingIndicator)
+        initialization()
+    }
+
+    private func initialization() {
         title = "Flickr Image App"
-        self.view.addSubview(loadingIndicator)
-        loadingIndicator.hidesWhenStopped = true
-        loadingIndicator.center = self.view.center
-        fetchFlickrImages(tags: tags)
-    }
-
-    //Mark:- Clearing old data to feed new data
-    
-    func resetImagesData() {
-        imageData.removeAll()
-        tags = ""
-        pageCount = 0
-        self.collectionView.reloadData()
-    }
-
-    func fetchFlickrImages(tags: String) {
-        self.pageCount += 1
-        loadingIndicator.startAnimating()
-        fetchImages.fetchImageDataFromFlickr(tags, pageCount) { [weak self] (result) in
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let photoInfo):
-                    self?.imageData.append(contentsOf: photoInfo.photos?.photo ?? [])
-                    self?.loadingIndicator.stopAnimating()
-                    self?.collectionView.reloadData()
-                case .failure(let error):
-                    print(error)
-                }
-            }
-        }
+        viewModel.loadingIndicator.hidesWhenStopped = true
+        viewModel.loadingIndicator.center = self.view.center
+        fetchFlickrImages(tags: viewModel.tags)
     }
 
     // MARK: UICollectionViewDataSource
@@ -59,36 +30,34 @@ class FlickrCollectionViewController: UICollectionViewController {
         return 1
     }
 
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageData.count
+        return viewModel.imageData.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let data = imageData[indexPath.row]
+        let data = viewModel.imageData[indexPath.row]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: viewModel.constants.reuseIdentifier, for: indexPath) as! FlickrCollectionViewCell
 
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: constants.reuseIdentifier, for: indexPath) as! FlickrCollectionViewCell
-
-        guard imageData.count != 0 else {
+        guard viewModel.imageData.count != 0 else {
             return cell
         }
 
-        if indexPath.row == imageData.count - 1 {
-            fetchFlickrImages(tags: tags)
+        if indexPath.row == viewModel.imageData.count - 10 {
+            fetchFlickrImages(tags: viewModel.tags)
         }
 
-        fetchImages.fetchImageSize (data.id) { (result) in
+        viewModel.fetchImages.fetchImageSize (data.id) { (result) in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let urlInfo):
                     let sourceImage = urlInfo.sizes?.size[1].source
                     guard let urlPath = sourceImage else { return }
-                    let image = self.imageSource.cache.object(forKey: URL(string: urlPath)! as NSURL)
+                    let image = self.viewModel.imageSource.cache.object(forKey: URL(string: urlPath)! as NSURL)
                     cell.imageFlickr.backgroundColor = UIColor(white: 0.95, alpha: 1)
                     cell.imageFlickr.image = image
                     if image == nil {
                         guard let url = URL(string: urlPath) else { return }
-                        self.imageSource.getImagesFromURL(from: url) { (result) in
+                        self.viewModel.imageSource.getImagesFromURL(from: url) { (result) in
                             DispatchQueue.main.async {
                                 switch result {
                                 case .success(let image):
@@ -111,27 +80,50 @@ class FlickrCollectionViewController: UICollectionViewController {
     }
 }
 
+//Mark:- Fetch image data from API
+
+extension FlickrCollectionViewController {
+    func fetchFlickrImages(tags: String) {
+        self.viewModel.pageCount += 1
+        viewModel.loadingIndicator.startAnimating()
+        viewModel.fetchImages.fetchImageDataFromFlickr(tags, viewModel.pageCount) { [weak self] (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let photoInfo):
+                    self?.viewModel.imageData.append(contentsOf: photoInfo.photos?.photo ?? [])
+                    self?.viewModel.loadingIndicator.stopAnimating()
+                    self?.collectionView.reloadData()
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+    }
+}
+
 //Mark:- UISearchBarDelegate
 
 extension FlickrCollectionViewController: UISearchBarDelegate {
 
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let searchBarView: UICollectionReusableView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: constants.reuseIdentifierSearch, for: indexPath)
+        let searchBarView: UICollectionReusableView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: viewModel.constants.reuseIdentifierSearch, for: indexPath)
         return searchBarView
     }
 
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
-        resetImagesData()
-        //check if search bar text is empty
+        viewModel.resetImagesData
         guard !(searchBar.text?.isEmpty ?? true) else {
             return
         }
-        tags = searchBar.text?.removeSpaceFromString ?? "kitten"
+        viewModel.tags = searchBar.text?.removeSpaceFromString ?? "kitten"
         fetchFlickrImages(tags: searchBar.text ?? "kitten")
         searchBar.text = ""
+        self.collectionView.reloadData()
     }
 }
+
+//Mark:- Segue Full Screen
 
 extension FlickrCollectionViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -140,7 +132,7 @@ extension FlickrCollectionViewController {
             let cell = sender as! UICollectionViewCell
             let indexPath = self.collectionView.indexPath(for: cell)
             if let indexPath = indexPath {
-                let photoID = imageData[indexPath.row].id
+                let photoID = viewModel.imageData[indexPath.row].id
                 fullScreenVC.photoID = photoID
             }
         }
@@ -152,17 +144,14 @@ extension FlickrCollectionViewController {
 extension FlickrCollectionViewController: UICollectionViewDelegateFlowLayout {
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let padding = constants.sectionInsets.left * (constants.itemsPerRow + 1)
-        let width = view.frame.width - padding
-        let widthPerImage = width / constants.itemsPerRow
-        return CGSize(width: widthPerImage, height: widthPerImage)
+        return viewModel.numberOfRows(frameWidth: view.frame.width)
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return constants.sectionInsets
+        return viewModel.constants.sectionInsets
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return constants.sectionInsets.left
+        return viewModel.constants.sectionInsets.left
     }
 }
