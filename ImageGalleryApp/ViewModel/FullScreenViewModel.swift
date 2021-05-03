2b83fd373a2907ae7988c353c9b0b7f49ff374fa
Gurjit Singh <gurjit.singh.engineer@gmail.com>
2021-05-03 01:13:22 +0100

    Add full screen view model

---
+++ ImageGalleryApp/ViewModel/FullScreenViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FullScreenViewModel.swift
+//  ImageGalleryApp
+//
+//  Created by Gurjit Singh on 02/05/21.
+//
+
+import Foundation
+
+class FullScreenViewModel {
+    let fetchImages = FetchFlickrImages()
+    let imageSource = FlickrImageSource()
+}
